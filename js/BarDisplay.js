266f41dbfa881e8a97289b010081efb2c87c5827
Victor André Gris Costa <victorandr98@gmail.com>
2020-04-07 00:12:27 -0300

    Added animation to bubble sort 2

--- js/BarDisplay.js
+++ js/BarDisplay.js
@@ -13,9 +13,9 @@ class BarDisplay {
     let height = 300;
     let margin = {
       top: 5,
-      bottom: 5,
+      bottom: 30,
       left: 5,
-      right: 5,
+      right: 30,
     };
     this.svg = div.append("svg");
     this.chart = this.svg
@@ -32,6 +32,19 @@ class BarDisplay {
       .scaleLinear()
       .rangeRound([0, height - margin.top - margin.bottom]);
     this.color = d3.scaleSequential(d3.interpolateBlues);
+
+    this.xAxis = this.svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${height - margin.bottom})`);
+
+    this.yAxis = this.svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${width - margin.right},${height - margin.bottom})`
+      )
+      .append("g")
+      .attr("transform", "rotate(180)");
   }
 
   get data() {
@@ -45,6 +58,7 @@ class BarDisplay {
     let yDomain = [0, d3.max(values)];
     let colorDomain = yDomain.slice();
     colorDomain[0] -= 5; // Darker Start
+
     this.x.domain(values);
     this.y.domain(yDomain);
     this.color.domain(colorDomain);
@@ -53,23 +67,68 @@ class BarDisplay {
     const y = this.y;
     const color = this.color;
 
+    const t = this.svg.transition().duration(266);
+
     this.chart
       .selectAll("rect")
       .data(data, (d) => d.value)
       .join(
-        (enter) => enter.append("rect").style("mix-blend-mode", "multiply"),
-        (update) => update,
-        (exit) => exit.remove()
-      )
-      .attr("x", (d) => x(d.value))
-      .attr("y", (d) => y(yDomain[1]) - y(d.value))
-      .attr("width", this.x.bandwidth())
-      .attr("height", (d) => y(d.value))
-      .style("fill", (d) => {
-        if (d.color !== null) {
-          return d.color;
-        }
-        return color(d.value);
-      });
+        (enter) =>
+          enter
+            .append("rect")
+            .style("mix-blend-mode", "multiply")
+            .attr("x", (d) => x(d.value))
+            .attr("y", (d) => y(yDomain[1]))
+            .attr("width", x.bandwidth())
+            .attr("height", y(0))
+            .style("fill", color(0))
+            .call((rect) =>
+              rect
+                .transition(t)
+                .delay(550)
+                .attr("y", (d) => y(yDomain[1]) - y(d.value))
+                .attr("height", (d) => y(d.value))
+                .style("fill", (d) => {
+                  if (d.color !== null) {
+                    return d.color;
+                  }
+                  return color(d.value);
+                })
+            ),
+        (update) =>
+          update.call((rect) =>
+            rect
+              .transition(t)
+              .delay(250)
+              .attr("x", (d) => x(d.value))
+              .attr("y", (d) => y(yDomain[1]) - y(d.value))
+              .attr("width", x.bandwidth())
+              .attr("height", (d) => y(d.value))
+              .style("fill", (d) => {
+                if (d.color !== null) {
+                  return d.color;
+                }
+                return color(d.value);
+              })
+          ),
+        (exit) =>
+          exit.call((rect) =>
+            rect
+              .transition(t)
+              .attr("y", y(yDomain[1]))
+              .attr("height", y(0))
+              .style("fill", color(0))
+              .remove()
+          )
+      );
+    this.xAxis.transition(t).delay(250).call(d3.axisBottom(x));
+    this.yAxis
+      .transition(t)
+      .delay(250)
+      .call(d3.axisLeft(y).ticks(d3.min([yDomain[1], 20])))
+      .selectAll("text")
+      .attr("x", 8)
+      .style("text-anchor", "start")
+      .attr("transform", "rotate(180)");
   }
 }
